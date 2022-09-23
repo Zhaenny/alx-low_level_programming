2e397987f67222eca352c6a9026ab415fa5f4bcd
Zainab <agboolazainab81@gmail.com>
2022-09-23 09:10:11 -0700

    prints a line of words with an escape of a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints some words wih an escape of a new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,, \n");
+	return (0);
+}
