aab639e19d51683ee5d57b710fcb180ec9333625
Zainab <agboolazainab81@gmail.com>
2022-09-23 09:43:45 -0700

    Printa variable types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int d;
 	float e;
 	
-	printf("Size of a char: %zu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %zu byte(s)\n", (unsigned long)sizeof(b));
-	printf("Size of a long int: %zu byte(s)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %zu byte(s)\n", (unsigned long)sizeof(d));
-	print("Size of a float: %zu byte(s)\n", %sizeof(e));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	print("Size of a float: %lu byte(s)\n", %sizeof(e));
 	
 	return (0);
 }
