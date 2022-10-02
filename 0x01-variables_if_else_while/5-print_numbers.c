5c498f0c2d79dd5191090a7d6f801f7d6591bb5e
Zainab <agboolazainab81@gmail.com>
2022-10-02 16:05:49 -0700

    Prints one digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always success 0
+ *
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (num < 10; num++)
+		printf("%d\n", num);
+	return (0);
+}
