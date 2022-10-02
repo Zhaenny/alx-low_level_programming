32b9219dc6f5b2bcf5cbb63cbb3a6081ad6fdad1
Zainab <agboolazainab81@gmail.com>
2022-10-02 16:24:26 -0700

    single digit numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always success 0
+ *
+ */
+int main(void)
+{
+	int n = 0;
+	for (; n < 10; n++)
+		putchar('0' + n);
+	putchar('\n');
+	return (0);
+}
