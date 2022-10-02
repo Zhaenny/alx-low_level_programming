d96256767ea197c17f5e144a6e70bd6c98e70754
Zainab <agboolazainab81@gmail.com>
2022-10-02 08:27:16 -0700

    Assigned variables

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,12 +20,12 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastnumber);
 	}
 	else if (lastnumber == 0)
-		{
+	{
 			printf("Last digit of %d is 0 and is 0\n", n);
-		}
-		else
-		{
+	}
+	else
+	{
 			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastnumber);
-		}
+	}
 	return (0);
 }
