53506d3c8a016b4ab97b55949c8c44d556c1481b
Zainab <agboolazainab81@gmail.com>
2022-10-06 10:32:08 -0700

    Allocates memory

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * malloc_checked - allocates memory using malloc
  * @b: size of memory to be allocated
+ *
  * Return: pointer to the allocated memory (SUCCESS)
  * 98 if insufficient memory was available (FAILURE)
  */
